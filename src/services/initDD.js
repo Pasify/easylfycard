8a31e48779eb08db28959698b46652383d22bacf
paskkal.io <pascalobiora73@gmail.com>
2024-10-19 00:42:05 +0100

    reset env secrete key

--- src/services/initDD.js
+++ src/services/initDD.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 // import { env } from "process";
 
-const KEY = import.meta.VITE_PAYSTACK_TEST_SECRETE_KEY;
+const KEY = import.meta.env.VITE_PAYSTACK_TEST_SECRETE_KEY;
+
 async function initializeDirectDebit() {
   try {
     const response = await axios.post(
@@ -10,18 +11,13 @@ async function initializeDirectDebit() {
         email: "ifesinachiobiora73@gmail.com",
         channel: "direct_debit",
         callback_url: "https://easylyfcard.vercel.app/api/payment-cb",
-      },
-      {
-        // headers: {
-        //   Authorization: `Bearer ${KEY}`,
-        //   "Content-Type": "application/json",
-        // },
       }
     );
-    console.log("Payment Initialized:", response.data);
+    if (response) {
+      console.log("Payment Initialized:", response.data);
+    }
   } catch (err) {
     console.error("Error initializing payment:", err);
-    console.log(err);
   }
 }
 export default initializeDirectDebit;
