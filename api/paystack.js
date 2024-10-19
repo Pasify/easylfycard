825c05f73cecee3f465e13425feffa44ae283b91
paskkal.io <pascalobiora73@gmail.com>
2024-10-19 21:40:42 +0100

    proper reference of key variable in backend code

--- api/paystack.js
+++ api/paystack.js
@@ -2,7 +2,7 @@ import https from "https";
 // import { env } from "process";
 
 export default function initializePayment(req, res) {
-  const KEY = process.env.PAYSTACK_TEST_SECRET_KEY;
+  const KEY = process.env.VITE_PAYSTACK_TEST_SECRET_KEY;
   console.log(`Paystack Secret Key:`, KEY);
   // Set CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*"); // Allow any origin
