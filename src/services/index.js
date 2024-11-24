641b4c1ca7e46b01bc473da010f7029b70806185
paskkal.io <pascalobiora73@gmail.com>
2024-11-24 22:15:44 +0100

    reset file export on error module

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 export { default as addUserAuthDetails } from "./addUserAuthDetails";
 export { default as chargeAccount } from "./addUserAuthDetails";
 export { default as createNewUser } from "./createNewUser";
-export { default as initDD } from "./initDD";
+export { default as initializeDirectDebit } from "./initDD";
 export { default as submitRegistration } from "./submitRegistration";
 export { default as testdb } from "./testdb";
 export { default as verifyAuth } from "./verifyAuth";
